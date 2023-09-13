9940c9113dfabc970ad4d1501629afb6e05e0f80
Richard Franks <richard.franks@securenetcoms.co.uk>
2023-09-13 11:47:23 +0100

    .

--- output.tf
+++ output.tf
@@ -4,10 +4,3 @@ output "subnet_v4" {
 output "subnet_v6" {
   value     = phpipam_first_free_subnet.subnet_v6
 }
-
-output "addresses_v4" {
-  value     = phpipam_first_free_address.addresses_v4
-}
-output "addresses_v6" {
-  value     = phpipam_first_free_address.addresses_v6
-}
